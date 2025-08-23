21febfb3ad380b1cb4ed70843ec953dd52cb05dd
Ahmed Abdulrahim <hemo089@outlook.com>
2025-08-23 20:30:19 +0300

    Add Challenges From 6 to 9

--- 01FundemntalPart01/Scripts/scripts.js
+++ 01FundemntalPart01/Scripts/scripts.js
@@ -1,4 +1,33 @@
-let js = "Amazing";
-if(js === "Amazing") alert("Hello to js");
-4+8+9+98;
-console.log(4+8+9+98);
+///6
+let country = "Egypt";
+let continent = "Africa";
+let population = 10;
+console.log(country);
+console.log(continent);
+console.log(population);
+//---------------------------------------------------------
+//7-DataType
+let isIsland = "Egypt";
+isIsland = true;
+let language;
+console.log(typeof country);
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof language);
+//---------------------------------------------------------
+//8- let, const, var
+language = "Arabic";
+const country1 = "Egypt";
+//country1="sdsada";//Eroor
+console.log(country1);
+//---------------------------------------------------------
+//9- Basic Operators
+//If your country split in half, and each half would contain half the population,then how many people would live in each half?
+let fHalf = population/2;
+//Increase the population of your country by 1 and log the result to the console
+fHalf++;
+//Finland has a population of 6 million. Does your country have more people than Finland?
+if (fHalf>6) alert("bigger than");
+if (fHalf<6) alert("lessthan than");
+let description = "Portugal is in Europe, and its 11 million people speak portuguese"
+//----------------------------------------------------------
