ebc4d6e46047b67585934d0b4e53584622b01dac
Ahmed Abdulrahim <hemo089@outlook.com>
2025-08-30 15:10:35 +0300

    Add Events to Our projects

--- 05-Modal/script.js
+++ 05-Modal/script.js
@@ -1,8 +1,18 @@
 'use strict';
 const modal = document.querySelector('.modal');
-const overLay = document.querySelector('.overLay');
+const overLay = document.querySelector('.overlay');
 const showModal = document.querySelectorAll('.show-modal'); // querySelectorAll
 const closeModal = document.querySelector('.close-modal');
 
+const AddHiddenClass = function () {
+  modal.classList.add('hidden');
+  overLay.classList.add('hidden');
+};
+
 for (let i = 0; i < showModal.length; i++)
-  console.log(showModal[i].textContent);
+  showModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overLay.classList.remove('hidden');
+  });
+closeModal.addEventListener('click', AddHiddenClass);
+overLay.addEventListener('click', AddHiddenClass);
