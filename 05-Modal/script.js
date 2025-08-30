232fae02a3c6e51d9606b5ebf4b72e9e51e0d64d
Ahmed Abdulrahim <hemo089@outlook.com>
2025-08-30 12:58:50 +0300

    Add project Modal

---
+++ 05-Modal/script.js
@@ -0,0 +1,8 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overLay = document.querySelector('.overLay');
+const showModal = document.querySelectorAll('.show-modal'); // querySelectorAll
+const closeModal = document.querySelector('.close-modal');
+
+for (let i = 0; i < showModal.length; i++)
+  console.log(showModal[i].textContent);
