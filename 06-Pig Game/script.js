5d0aaa21994d2b5f907b439a1f5380e191106b97
Ahmed Abdulrahim <hemo089@outlook.com>
2025-08-30 20:05:17 +0300

    Switch Player && change the bgColor of Active Player

--- 06-Pig Game/script.js
+++ 06-Pig Game/script.js
@@ -5,13 +5,17 @@ const diceElement = document.querySelector('.dice');
 const roolDice = document.querySelector('.btn--roll');
 const newDice = document.querySelector('.btn--new');
 const holdDice = document.querySelector('.btn--hold');
-const Score0 = document.getElementById('current--0');
-const score1 = document.getElementById('current--1');
+const playerScore0 = document.getElementById('current--0');
+const playerScore1 = document.getElementById('current--1');
+const active0 = document.querySelector('.player--0');
+const active1 = document.querySelector('.player--1');
 
 score0Element.textContent = 0;
 score1Element.textContent = 0;
 diceElement.classList.add('hidden');
-let Score = 0;
+let currentScore = 0;
+//active Player
+let activePlayer = 0;
 //Add Roll Functionality
 roolDice.addEventListener('click', function () {
   //Genarting random Number
@@ -21,10 +25,16 @@ roolDice.addEventListener('click', function () {
   diceElement.src = `dice-${diceNumber}.png`;
   //Add Logic
   if (diceNumber !== 1) {
-    Score += diceNumber;
-    Score0.textContent = Score;
+    currentScore += diceNumber;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
-    Score = 0;
-    Score0.textContent = Score;
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    //toggle th class if found remove it and the oppiste
+    active0.classList.toggle('player--active');
+    active1.classList.toggle('player--active');
   }
 });
