d06b1c500d763e8090196c345e2b408e5775817a
Ahmed Abdulrahim <hemo089@outlook.com>
2025-09-01 21:06:40 +0300

    Hoisting

--- 07-Behind The Scenes/script.js
+++ 07-Behind The Scenes/script.js
@@ -1,1 +1,12 @@
 'use strict';
+const getAge = function (birthYear) {
+  const age = 2025 - birthYear;
+  console.log(firstName);
+  function Details() {
+    console.log(`the name is ${firstName} , age is ${age}`); //hoisting
+  }
+  Details();
+  return age;
+};
+const firstName = 'Ahmed'; //hoisting
+getAge(2001);
