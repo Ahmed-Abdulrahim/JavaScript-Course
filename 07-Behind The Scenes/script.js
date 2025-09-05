b8ca2395f614447bea0524895630c5339b255a25
Ahmed Abdulrahim <hemo089@outlook.com>
2025-09-05 15:03:52 +0300

    Practice Hoisting Examples

--- 07-Behind The Scenes/script.js
+++ 07-Behind The Scenes/script.js
@@ -1,5 +1,5 @@
 'use strict';
-const getAge = function (birthYear) {
+/*const getAge = function (birthYear) {
   const age = 2025 - birthYear;
   console.log(firstName);
   function Details() {
@@ -9,4 +9,25 @@ const getAge = function (birthYear) {
   return age;
 };
 const firstName = 'Ahmed'; //hoisting
-getAge(2001);
+getAge(2001);*/
+//----------------------------------------------------------Hoisting && TDZ
+console.log(_name); // Undeifend
+//console.log(age); //cannot access 'age' before initialization
+//console.log(city); //Cannot access 'CITY' before initialization
+var _name = 'Ahmed';
+let age = 24;
+const city = 'mahalla';
+//------------------------------------------------------------------- function
+console.log(getAge(2001)); //24
+console.log(getName('Ahmed')); //Cannot access 'getName' before initialization
+console.log(getCity('mahalla')); //Cannot access 'getCity' before initialization
+function getAge(birthYear) {
+  return 2025 - birthYear;
+}
+
+let getName = function (_name) {
+  return _name;
+};
+
+const getCity = city => city;
+//---------------------------------------------------------------------
