708cced33111e2a2a1807777ba9e6a18febd660d
Ahmed Abdulrahim <hemo089@outlook.com>
2025-09-06 15:13:01 +0300

    This Keyword

--- 07-Behind The Scenes/script.js
+++ 07-Behind The Scenes/script.js
@@ -11,7 +11,7 @@
 const firstName = 'Ahmed'; //hoisting
 getAge(2001);*/
 //----------------------------------------------------------Hoisting && TDZ
-console.log(_name); // Undeifend
+/*console.log(_name); // Undeifend
 //console.log(age); //cannot access 'age' before initialization
 //console.log(city); //Cannot access 'CITY' before initialization
 var _name = 'Ahmed';
@@ -29,5 +29,31 @@ let getName = function (_name) {
   return _name;
 };
 
-const getCity = city => city;
-//---------------------------------------------------------------------
+const getCity = city => city;*/
+//-------------------------------------------------------------this KeyWord
+console.log(this); // window Object
+/*function calcAge(birthYear) {
+  console.log(2025 - birthYear);
+  console.log(this);
+}
+///arrow Function
+var calcAge2 = birthYear => {
+  console.log(2025 - birthYear);
+  console.log(this); // the parent of this => window Object
+};*/
+//calcAge2(2001);
+const ahmed = {
+  _name: 'ahmed',
+  age: 24,
+  calcYear: function () {
+    console.log(this);
+    console.log(2025 - this.age);
+  },
+};
+ahmed.calcYear();
+const mohamed = {
+  _name: 'mohamed',
+  age: 323,
+};
+mohamed.calcYear = ahmed.calcYear; // Method Borrowing
+mohamed.calcYear();
